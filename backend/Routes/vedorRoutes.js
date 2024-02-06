e5da092f71f671fc94c601ea8cd257b1211e43b9
Sonalika Bisoyi <bisoyisonalika@gmail.com>
2024-02-07 00:49:42 +0530

    OTP

--- backend/Routes/vedorRoutes.js
+++ backend/Routes/vedorRoutes.js
@@ -6,6 +6,8 @@ const vendorController = require('../Controllers/vendorController');
 // Signup route for vendors
 router.post('/signup', vendorController.signup);
 router.post('/login', vendorController.login);
+router.post('/verify-otp', vendorController.verifyOTP);
+
 
 
 module.exports = router;
